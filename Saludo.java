e9c65d7bf51c11d80ba968eb18eb3a553db04afb
Javier Villaseñor García <javiervg0300@gmail.com>
2024-09-25 12:44:09 +0200

    Update Saludo.java

--- Saludo.java
+++ Saludo.java
@@ -2,5 +2,7 @@ public class Saludo {
     public void saludar() {
         System.out.println("¡Hola, mundo!");
     }
-
+    public void despedir() {
+        System.out.println("¡Adiós, mundo!");
+    }
 }
